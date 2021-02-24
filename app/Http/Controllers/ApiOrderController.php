9bc68fe3d00ebd1a3a26150fe6a25a2befea566e
sach2323 <sachinsingh328@gmail.com>
2021-02-24 14:31:52 +0530

    refactored code

---
+++ app/Http/Controllers/ApiOrderController.php
@@ -0,0 +1,287 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\Logs;
+use App\PPClient;
+use Exception;
+use Illuminate\Http\Request;
+use PayPalCheckoutSdk\Orders\OrdersCreateRequest;
+use PayPalCheckoutSdk\Orders\OrdersCaptureRequest;
+use PayPalCheckoutSdk\Orders\OrdersGetRequest;
+use PayPalHttp\HttpRequest;
+
+
+class WebhookCreateRequest extends HttpRequest
+{
+    function __construct()
+    {
+        parent::__construct("/v1/notifications/webhooks", "POST");
+        $this->headers["Content-Type"] = "application/json";
+    }
+}
+
+
+class ApiOrderController extends Controller
+
+{
+    public function __construct()
+    {
+        $this->client = PPClient::client();
+        $this->out = new \Symfony\Component\Console\Output\ConsoleOutput();
+    }
+
+    public function createPaymentLink(Request $body)
+    {
+        try {
+            $order = new Order;
+            if ($body->convert == false) {
+                $amount = $body->amount;
+                $order->amount = $body->amount;
+            } else {
+                $calc = $body->conversion["minutes"] * $body->conversion["rate"];
+                $amount = number_format($calc, 2, '.', '');
+                $order->minutes = $body->conversion["minutes"];
+                $order->amount = $amount;
+            }
+            $request = new OrdersCreateRequest();
+            $request->prefer('return=representation');
+            $request->body = [
+                "intent" => "CAPTURE",
+                "purchase_units" => [[
+                    "reference_id" => "test_ref_id1",
+                    "amount" => [
+                        "value" => $amount,
+                        "currency_code" => "INR"
+                    ]
+                ]],
+                "application_context" => [
+                    "cancel_url" => "https://example.com/cancel",
+                    "return_url" => "https://example.com/return"
+                ]
+            ];
+
+
+
+            $response = $this->client->execute($request);
+
+            $orderId = $response->result->id;
+            $status = $response->result->status;
+            $this->out->writeln('payment link');
+            //creaing a log for just created order
+            $logs = new Logs;
+            $logs->order_id = $orderId;
+            $logs->status = $status;
+            $logs->meta = json_encode($response);
+            $logs->save();
+
+            $data = array(
+                "errors" => false,
+                "success" => true,
+                "message" => $status,
+                "data" => array(
+                    "order_id" => $orderId,
+                    "payment_url" => $response->result->links[1]->href,
+                    "meta" => $response
+                )
+            );
+
+            $order->id = $orderId;
+            $order->status = $status;
+            $order->meta = json_encode($response);
+            $order->payer_email = json_encode($response->result->purchase_units[0]->payee->email_address);
+            $order->payer_json = json_encode($response->result->purchase_units[0]->payee);
+            $order->save();
+
+            return $data;
+        } catch (Exception $ex) {
+            $data = array(
+                "errors" => true,
+                "success" => false,
+                "message" => $ex->getMessage(),
+                "data" => $ex
+            );
+
+            return $data;
+        }
+    }
+
+    public function getOrderById(Request $body)
+    {
+        if ($body->order_id) {
+            $orderId = $body->order_id;
+        }
+        $request = new OrdersGetRequest($orderId);
+        try {
+            $response = $this->client->execute($request);
+            $data = array(
+                "errors" => false,
+                "success" => true,
+                "message" => sprintf("Your Order Status is %s", $response->result->status),
+                "data" => array(
+                    "status" => $response->result->status,
+                    "meta" => $response
+                )
+            );
+
+            return $data;
+        } catch (Exception $ex) {
+            $data = array(
+                "errors" => true,
+                "success" => false,
+                "message" => $ex->getMessage(),
+                "data" => array(
+                    "meta" => $ex
+                )
+            );
+            return $data;
+        }
+    }
+
+    public function getOrderHistoryById(Request $body)
+    {
+        if ($body->order_id) {
+            $orderId = $body->order_id;
+        }
+        try {
+            if (Logs::where('order_id', $orderId)->exists()) {
+                $orderHistory = Logs::where('order_id', $orderId)->latest()->get();
+
+                $data = array(
+                    "errors" => false,
+                    "success" => true,
+                    "message" => sprintf(" Order Hisory fetched"),
+                    "data" => array(
+                        "status" => 200,
+                        "meta" => $orderHistory
+                    )
+                );
+
+                return $data;
+            } else {
+                $data = array(
+                    "errors" => true,
+                    "success" => false,
+                    "message" => "Order Id not found, please check your order id",
+                );
+                return $data;
+            }
+        } catch (Exception $ex) {
+            $data = array(
+                "errors" => true,
+                "success" => false,
+                "message" => $ex->getMessage(),
+                "data" => array(
+                    "meta" => $ex
+                )
+            );
+            return $data;
+        }
+    }
+
+    public function getOrderStatusById(Request $body)
+    {
+        if ($body->order_id) {
+            $orderId = $body->order_id;
+        }
+        $request = new OrdersCaptureRequest($orderId);
+        $request->prefer('return=representation');
+        try {
+            $response = $this->client->execute($request);
+            $data = array(
+                "errors" => false,
+                "success" => true,
+                "message" => sprintf("Your Order Status is %s", $response->result->status),
+                "data" => array(
+                    "status" => $response->result->status,
+                    "meta" => $response
+                )
+            );
+
+            return $data;
+        } catch (Exception $ex) {
+            $data = array(
+                "errors" => true,
+                "success" => false,
+                "message" => $ex->getMessage(),
+                "data" => array(
+                    "meta" => $ex
+                )
+            );
+            return $data;
+        }
+    }
+
+    public function createWebhook(Request $body)
+    {
+
+        try {
+            $request = new WebhookCreateRequest();
+            $request->body = $body->data;
+
+            $response = $this->client->execute($request);
+
+            $data = array(
+                "errors" => false,
+                "success" => true,
+                "message" => sprintf("Webhook created with id %s", $response->result->id),
+                "data" => array(
+                    "webhook_id" => $response->result->id,
+                    "webhook_url" => $response->result->url,
+                    "meta" => $response
+                )
+            );
+
+            return $data;
+        } catch (Exception $ex) {
+            $data = array(
+                "errors" => true,
+                "success" => false,
+                "message" => $ex->getMessage(),
+                "data" => $ex
+            );
+
+            return $data;
+        }
+    }
+
+    public function webhookOrderListener(Request $request)
+    {
+        try {
+            $this->out->writeln('Calling listener order webhook.');
+            $json = file_get_contents('php://input');
+            $action = json_decode($json, true);
+            $orderId = $action["resource"]["id"];
+            $status = $action["resource"]["status"];
+            $logs = new Logs;
+            $logs->order_id = $orderId;
+            $logs->status = $status;
+            $logs->meta = json_encode($action);
+            $logs->save();
+
+            if (Order::where('id', $orderId)->exists()) {
+                Order::where('id', $orderId)->update(['status' => $status]);
+            }
+
+            $data = array(
+                "errors" => false,
+                "success" => true,
+                "message" => "successfully logged and updated Orders",
+            );
+
+            return $data;
+        } catch (Exception $ex) {
+            $this->out->writeln('Error Calling listener order webhook.');
+            $data = array(
+                "errors" => true,
+                "success" => false,
+                "message" => $ex->getMessage(),
+                "data" => $ex
+            );
+
+            return $data;
+        }
+    }
+
+}
--- app/Http/Controllers/ApiSubscriptionController.php
+++ app/Http/Controllers/ApiSubscriptionController.php
@@ -2,56 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Order;
 use App\Models\Logs;
 use App\Models\Plan;
 use App\Models\Subscription;
+use App\PPClient;
 use Exception;
 use Illuminate\Http\Request;
-use PayPalCheckoutSdk\Orders\OrdersCreateRequest;
-use PayPalCheckoutSdk\Orders\OrdersCaptureRequest;
-use PayPalCheckoutSdk\Orders\OrdersGetRequest;
+
 use PayPalHttp\HttpRequest;
-
-use PayPalCheckoutSdk\Core\PayPalHttpClient;
-use PayPalCheckoutSdk\Core\SandboxEnvironment;
-
-ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-
-class PPClient
-{
-    /**
-     * Returns PayPal HTTP client instance with environment that has access
-     * credentials context. Use this instance to invoke PayPal APIs, provided the
-     * credentials have access.
-     */
-    public static function client()
-    {
-        return new PayPalHttpClient(self::environment());
-    }
-
-    /**
-     * Set up and return PayPal PHP SDK environment with PayPal access credentials.
-     * This sample uses SandboxEnvironment. In production, use LiveEnvironment.
-     */
-    public static function environment()
-    {
-        $clientId = getenv("PAYPAL_CLIENT_ID") ?: "AV--tXCNVtOQ-TenkEs8PxbKN3j80plkcEFY_9JrlRRyz5IlHdGl6WlCjyMB69DRmarvjTiym3HD8KAl";
-        $clientSecret = getenv("PAYPAL_CLIENT_SECRET") ?: "EL3Q7vbzFc1hbBp7IyMIEaw30T1itMI417O5PdLlOAs2EGDBr6wG2d9ZhUhw55SVvUQP6lfYqoChg9E3";
-        return new SandboxEnvironment($clientId, $clientSecret);
-    }
-}
-
-class WebhookCreateRequest extends HttpRequest
-{
-    function __construct()
-    {
-        parent::__construct("/v1/notifications/webhooks", "POST");
-        $this->headers["Content-Type"] = "application/json";
-    }
-}
 
 class PlansGetRequest extends HttpRequest
 {
@@ -179,8 +137,7 @@ class SubscriptionsReviseRequest extends HttpRequest
     }
 }
 
-
-class ApiController extends Controller
+class ApiSubscriptionController extends Controller
 
 {
     public function __construct()
@@ -189,319 +146,68 @@ class ApiController extends Controller
         $this->out = new \Symfony\Component\Console\Output\ConsoleOutput();
     }
 
-    public function createPaymentLink(Request $body)
-    {
-        try {
-            $order = new Order;
-            if ($body->convert == false) {
-                $amount = $body->amount;
-                $order->amount = $body->amount;
-            } else {
-                $calc = $body->conversion["minutes"] * $body->conversion["rate"];
-                $amount = number_format($calc, 2, '.', '');
-                $order->minutes = $body->conversion["minutes"];
-                $order->amount = $amount;
+
+    public function getPlanDetailsById(Request $body)
+        {
+            if ($body->plan_id) {
+                $planId = $body->plan_id;
             }
-            $request = new OrdersCreateRequest();
-            $request->prefer('return=representation');
-            $request->body = [
-                "intent" => "CAPTURE",
-                "purchase_units" => [[
-                    "reference_id" => "test_ref_id1",
-                    "amount" => [
-                        "value" => $amount,
-                        "currency_code" => "INR"
-                    ]
-                ]],
-                "application_context" => [
-                    "cancel_url" => "https://example.com/cancel",
-                    "return_url" => "https://example.com/return"
-                ]
-            ];
-
-
-
-            $response = $this->client->execute($request);
-
-            $orderId = $response->result->id;
-            $status = $response->result->status;
-            $this->out->writeln('payment link');
-            //creaing a log for just created order
-            $logs = new Logs;
-            $logs->order_id = $orderId;
-            $logs->status = $status;
-            $logs->meta = json_encode($response);
-            $logs->save();
-
-            $data = array(
-                "errors" => false,
-                "success" => true,
-                "message" => $status,
-                "data" => array(
-                    "order_id" => $orderId,
-                    "payment_url" => $response->result->links[1]->href,
-                    "meta" => $response
-                )
-            );
-
-            $order->id = $orderId;
-            $order->status = $status;
-            $order->meta = json_encode($response);
-            $order->payer_email = json_encode($response->result->purchase_units[0]->payee->email_address);
-            $order->payer_json = json_encode($response->result->purchase_units[0]->payee);
-            $order->save();
-
-            return $data;
-        } catch (Exception $ex) {
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => $ex
-            );
-
-            return $data;
-        }
-    }
-
-    public function getOrderById(Request $body)
-    {
-        if ($body->order_id) {
-            $orderId = $body->order_id;
-        }
-        $request = new OrdersGetRequest($orderId);
-        try {
-            $response = $this->client->execute($request);
-            $data = array(
-                "errors" => false,
-                "success" => true,
-                "message" => sprintf("Your Order Status is %s", $response->result->status),
-                "data" => array(
-                    "status" => $response->result->status,
-                    "meta" => $response
-                )
-            );
-
-            return $data;
-        } catch (Exception $ex) {
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => array(
-                    "meta" => $ex
-                )
-            );
-            return $data;
-        }
-    }
-
-    public function getOrderHistoryById(Request $body)
-    {
-        if ($body->order_id) {
-            $orderId = $body->order_id;
-        }
-        try {
-            if (Logs::where('order_id', $orderId)->exists()) {
-                $orderHistory = Logs::where('order_id', $orderId)->latest()->get();
-
+            $request = new PlansGetRequest($planId);
+            try {
+                $response = $this->client->execute($request);
                 $data = array(
                     "errors" => false,
                     "success" => true,
-                    "message" => sprintf(" Order Hisory fetched"),
+                    "message" => sprintf("Fetched Plan detail"),
                     "data" => array(
-                        "status" => 200,
-                        "meta" => $orderHistory
+                        "status" => $response->result->status,
+                        "meta" => $response
                     )
                 );
 
                 return $data;
-            } else {
+            } catch (Exception $ex) {
                 $data = array(
                     "errors" => true,
                     "success" => false,
-                    "message" => "Order Id not found, please check your order id",
+                    "message" => $ex->getMessage(),
+                    "data" => array(
+                        "meta" => $ex
+                    )
                 );
                 return $data;
             }
-        } catch (Exception $ex) {
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => array(
-                    "meta" => $ex
-                )
-            );
-            return $data;
         }
-    }
 
-    public function getOrderStatusById(Request $body)
-    {
-        if ($body->order_id) {
-            $orderId = $body->order_id;
-        }
-        $request = new OrdersCaptureRequest($orderId);
-        $request->prefer('return=representation');
-        try {
-            $response = $this->client->execute($request);
-            $data = array(
-                "errors" => false,
-                "success" => true,
-                "message" => sprintf("Your Order Status is %s", $response->result->status),
-                "data" => array(
-                    "status" => $response->result->status,
-                    "meta" => $response
-                )
-            );
+        public function getAllPlans(Request $body)
+        {
 
-            return $data;
-        } catch (Exception $ex) {
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => array(
-                    "meta" => $ex
-                )
-            );
-            return $data;
-        }
-    }
+            $request = new GetAllPlansRequest();
+            try {
+                $response = $this->client->execute($request);
+                $data = array(
+                    "errors" => false,
+                    "success" => true,
+                    "message" => sprintf("Fetched All Plans"),
+                    "data" => array(
+                        "status" => $response,
+                        "meta" => $response
+                    )
+                );
 
-    public function createWebhook(Request $body)
-    {
-
-        try {
-            $request = new WebhookCreateRequest();
-            $request->body = $body->data;
-
-            $response = $this->client->execute($request);
-
-            $data = array(
-                "errors" => false,
-                "success" => true,
-                "message" => sprintf("Webhook created with id %s", $response->result->id),
-                "data" => array(
-                    "webhook_id" => $response->result->id,
-                    "webhook_url" => $response->result->url,
-                    "meta" => $response
-                )
-            );
-
-            return $data;
-        } catch (Exception $ex) {
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => $ex
-            );
-
-            return $data;
-        }
-    }
-
-    public function webhookOrderListener(Request $request)
-    {
-        try {
-            $this->out->writeln('Calling listener order webhook.');
-            $json = file_get_contents('php://input');
-            $action = json_decode($json, true);
-            $orderId = $action["resource"]["id"];
-            $status = $action["resource"]["status"];
-            $logs = new Logs;
-            $logs->order_id = $orderId;
-            $logs->status = $status;
-            $logs->meta = json_encode($action);
-            $logs->save();
-
-            if (Order::where('id', $orderId)->exists()) {
-                Order::where('id', $orderId)->update(['status' => $status]);
+                return $data;
+            } catch (Exception $ex) {
+                $data = array(
+                    "errors" => true,
+                    "success" => false,
+                    "message" => $ex->getMessage(),
+                    "data" => array(
+                        "meta" => $ex
+                    )
+                );
+                return $data;
             }
-
-            $data = array(
-                "errors" => false,
-                "success" => true,
-                "message" => "successfully logged and updated Orders",
-            );
-
-            return $data;
-        } catch (Exception $ex) {
-            $this->out->writeln('Error Calling listener order webhook.');
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => $ex
-            );
-
-            return $data;
         }
-    }
-
-    public function getPlanDetailsById(Request $body)
-    {
-        if ($body->plan_id) {
-            $planId = $body->plan_id;
-        }
-        $request = new PlansGetRequest($planId);
-        try {
-            $response = $this->client->execute($request);
-            $data = array(
-                "errors" => false,
-                "success" => true,
-                "message" => sprintf("Fetched Plan detail"),
-                "data" => array(
-                    "status" => $response->result->status,
-                    "meta" => $response
-                )
-            );
-
-            return $data;
-        } catch (Exception $ex) {
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => array(
-                    "meta" => $ex
-                )
-            );
-            return $data;
-        }
-    }
-
-    public function getAllPlans(Request $body)
-    {
-
-        $request = new GetAllPlansRequest();
-        try {
-            $response = $this->client->execute($request);
-            $data = array(
-                "errors" => false,
-                "success" => true,
-                "message" => sprintf("Fetched All Plans"),
-                "data" => array(
-                    "status" => $response,
-                    "meta" => $response
-                )
-            );
-
-            return $data;
-        } catch (Exception $ex) {
-            $data = array(
-                "errors" => true,
-                "success" => false,
-                "message" => $ex->getMessage(),
-                "data" => array(
-                    "meta" => $ex
-                )
-            );
-            return $data;
-        }
-    }
 
     public function createPlan(Request $body)
     {
